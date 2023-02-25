bb697206e0b4e73f1d178cbf76f2cbd1689f999e
Rohitash Singh <93479842+Rohitashsingh89@users.noreply.github.com>
2023-02-25 10:35:40 +0530

    Update action.php

--- templates/action.php
+++ templates/action.php
@@ -1,10 +1,12 @@
 <?php
 
-if(isset($_POST)) {
-    $email = $_POST['email'];
-    $password = $_POST['pass'];
+echo ("hello world");
 
-}
-echo ($email. " and ".$password);
+// if(isset($_POST)) {
+//     $email = $_POST['email'];
+//     $password = $_POST['pass'];
+
+// }
+// echo ($email. " and ".$password);
 
 ?>
